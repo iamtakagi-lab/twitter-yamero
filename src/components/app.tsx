7b8b4472b1eaf02fb781444cbd9b88dddbb2dd20
takagi <iam.takagi.dev@gmail.com>
2021-09-04 03:46:15 +0900

    Add ❗

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,8 @@ import { TweetButton } from './tweet-button'
 
 export const App: React.FC<{}> = () => 
     <>
-        <p>Twitter やめろ</p>
-        <TweetButton text="Twitter やめろ" url="https://twitter-yamero.vercel.app" />
+        <p>Twitter やめろ❗</p>
+        <TweetButton text="Twitter やめろ❗" url="https://twitter-yamero.vercel.app" />
         <br />
         <a href="https://github.com/iamtakagi/twitter-yamero">https://github.com/iamtakagi/twitter-yamero</a> 
     </>
